d56e192207ce801605cff9d8789eb4226c9642bb
Chris Barbour <chris.barbour03@gmail.com>
2019-03-25 20:16:52 +0000

    allowing certificate arn to be passed

--- src/main/kotlin/io/hexlabs/kloudformation/module/s3/S3Website.kt
+++ src/main/kotlin/io/hexlabs/kloudformation/module/s3/S3Website.kt
@@ -36,7 +36,7 @@ data class S3Website(val bucket: Bucket, val policy: BucketPolicy? = null, val d
             certificateArn: Value<String>? = null,
             modifications: Modification<S3Distribution.Parts, S3Distribution, S3Distribution.Predefined>.() -> Unit = {}
         ) {
-            s3Distribution.invoke(S3Distribution.Props(domain, httpMethod, sslSupportMethod, priceClass,certificateArn), modifications)
+            s3Distribution.invoke(S3Distribution.Props(domain, httpMethod, sslSupportMethod, priceClass, certificateArn), modifications)
         }
         val s3Bucket = modification<Bucket.Builder, Bucket, BucketProps>()
         val s3BucketPolicy = optionalModification<BucketPolicy.Builder, BucketPolicy, PolicyProps>()
